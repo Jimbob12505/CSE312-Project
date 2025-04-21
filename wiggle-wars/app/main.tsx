fa3f973db6f14150eb4974a268230336cb70cd44
“Jimbob12505” <vatsal.12505@gmail.com>
2025-04-21 15:19:26 -0400

    Modified index and main to load base project

--- wiggle-wars/app/main.tsx
+++ wiggle-wars/app/main.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import routes from './routes'
+import Welcome from '/welcome/welcome'
 
 const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Welcome />
   </React.StrictMode>
 )
-
 
